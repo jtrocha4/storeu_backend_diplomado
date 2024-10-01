6a561d3e1f0baac8b0277cbc29b9011fdbf46390
jtrocha4 <jtrocha04@gmail.com>
2024-09-30 22:21:37 -0500

    update(controller): product

--- controller/product.js
+++ controller/product.js
@@ -2,37 +2,48 @@ require('../mongo')
 
 const express = require('express')
 const router = express.Router()
+const {uploadImg} = require('../middleware/storage')
 
 const Product = require('../models/Product')
 
-router.get('/products', async (req, res)=>{
-    const allProducts = await Product.find()
-    res.json(allProducts)
+router.get('/api/products', async (req, res)=>{
+    data = await Product.find()
+    res.json(data)
+})
+
+router.get('/api/products/:id', async (req, res)=>{
+    const {id} = req.params
+    data = await Product.findById(id)
+    res.json(data)
+})
+
+router.post('/api/product', uploadImg.single('imgUrl') ,async(req, res)=>{
+    
+    const {nombre, imgUrl, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad} = req.body
+
+    try {
+        const product = new Product({
+            nombre, 
+            imgUrl: '/uploads/'+req.file.filename, 
+            descripcion, 
+            categoria, 
+            talla, 
+            color, 
+            fit, 
+            genero, 
+            temporada, 
+            precio, 
+            cantidad
+        })
+
+        const createProduct = await product.save()
+        res.status(201).json(createProduct)
+
+    } catch (error) {
+        console.error("Error al crear el producto:", error);
+        res.status(500).json({ error: error.message })
+    }
+
 })
 
 module.exports = router
-
-
-// const product = new Product({
-//     nombre: "Pantalones vaqueros",
-//     descripcion: "Jeans de mezclilla con diseño ajustado y elástico.",
-//     categoria: "Pantalones",
-//     talla: "32",
-//     color: "Azul oscuro",
-//     fit: "Slim",
-//     genero: "Hombre",
-//     temporada: "Invierno",
-//     precio: 39.99,
-//     cantidad: 30
-// })
-
-// const createProduct = async() =>{
-//     try {
-//         const res = await product.save()
-//         console.log(res)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-// createProduct()
