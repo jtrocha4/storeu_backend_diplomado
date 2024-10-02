f2921cb5da92a3c6cc054d147f04c6a6ab5fa095
jtrocha4 <jtrocha04@gmail.com>
2024-10-01 22:10:01 -0500

    update(controller): product. Method put and delete

--- controller/product.js
+++ controller/product.js
@@ -18,11 +18,10 @@ router.get('/api/products/:id', async (req, res)=>{
 })
 
 router.post('/api/product', uploadImg.single('imgUrl') ,async(req, res)=>{
-    
-    const {nombre, imgUrl, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad} = req.body
-
     try {
-        const product = new Product({
+        const {nombre, imgUrl, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad} = req.body
+
+        const data = new Product({
             nombre, 
             imgUrl: '/uploads/'+req.file.filename, 
             descripcion, 
@@ -36,7 +35,7 @@ router.post('/api/product', uploadImg.single('imgUrl') ,async(req, res)=>{
             cantidad
         })
 
-        const createProduct = await product.save()
+        const createProduct = await data.save()
         res.status(201).json(createProduct)
 
     } catch (error) {
@@ -44,6 +43,50 @@ router.post('/api/product', uploadImg.single('imgUrl') ,async(req, res)=>{
         res.status(500).json({ error: error.message })
     }
 
+})
+
+router.put('/api/product/:id', uploadImg.single('imgUrl') ,async(req, res)=>{
+    try {
+        const id = req.params.id
+        const {nombre, imgUrl, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad, estado} = req.body
+        const data = {
+            nombre, 
+            imgUrl: '/uploads/'+req.file.filename || '', 
+            descripcion, 
+            categoria, 
+            talla, 
+            color, 
+            fit, 
+            genero, 
+            temporada, 
+            precio, 
+            cantidad,
+            estado
+        }
+
+        console.log(id)
+        console.log(req.body)
+
+        // const updateProduct = await Product.findByIdAndUpdate(id, data, {new: true})
+        const updateProduct = await Product.findByIdAndUpdate(id, data, { new: true })
+        res.status(200).json(updateProduct)
+
+    } catch (error) {
+        console.error("Error al crear el producto:", error);
+        res.status(500).json({ error: error.message })
+    }
+})
+
+router.delete('/api/product/:id', async(req, res)=>{
+    try {
+        const id = req.params.id 
+
+        const deleteProduct = await Product.findByIdAndDelete(id)
+        res.status(200).json(deleteProduct)
+    } catch (error) {
+        console.error("Error al crear el producto:", error);
+        res.status(500).json({ error: error.message })
+    }
 })
 
 module.exports = router
