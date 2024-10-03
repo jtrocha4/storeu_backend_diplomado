ce9d79e9e4f61204666e87af1629883e39d6cd6a
jtrocha4 <jtrocha04@gmail.com>
2024-10-02 21:19:27 -0500

    update: index

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.use(express.json())
 app.use(express.static('public'))
 
 const productController = require('./controller/product')
+const userController = require('./controller/user')
 
 app.get('/', (req, res)=>{
     res.send('<h1>Storeu-Api</h1>')
 })
 
 app.use('/', productController)
+app.use('/', userController)
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT)
