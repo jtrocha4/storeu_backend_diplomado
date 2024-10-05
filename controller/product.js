9179f56fdd5533a5970b1a624d4bde3bd1316b27
jtrocha4 <jtrocha04@gmail.com>
2024-10-05 10:19:45 -0500

    updated(controller): product. Protected Petitions

--- controller/product.js
+++ controller/product.js
@@ -2,7 +2,9 @@ require('../mongo')
 
 const express = require('express')
 const router = express.Router()
+
 const { uploadImg } = require('../middleware/storage')
+const authorizationVerification = require('../middleware/authorizationVerification')
 
 const Product = require('../models/Product')
 const User = require('../models/User')
@@ -19,74 +21,86 @@ router.get('/api/products/:id', async (req, res) => {
   res.json(data)
 })
 
-router.post('/api/product', uploadImg.single('imgUrl'), async (req, res) => {
+router.post('/api/product', authorizationVerification, uploadImg.single('imgUrl'), async (req, res) => {
   try {
-    const { nombre, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad, userId } = req.body
+    const { nombre, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad } = req.body
 
-    const user = await User.findById(userId)
+    const userId = req.userId
+    const userRole = req.userRole
 
-    const data = new Product({
-      nombre,
-      imgUrl: '/uploads/' + req.file.filename,
-      descripcion,
-      categoria,
-      talla,
-      color,
-      fit,
-      genero,
-      temporada,
-      precio,
-      cantidad,
-      usuario: user._id
-    })
+    if (userRole === 'Administrador') {
+      const user = await User.findById(userId)
 
-    const createProduct = await data.save()
-    user.productos = user.productos.concat(data._id)
-    await user.save()
-    res.status(201).json(createProduct)
-  } catch (error) {
-    console.error('Error al crear el producto:', error)
-    res.status(500).json({ error: error.message })
-  }
-})
+      const data = new Product({
+        nombre,
+        imgUrl: '/uploads/' + req.file.filename,
+        descripcion,
+        categoria,
+        talla,
+        color,
+        fit,
+        genero,
+        temporada,
+        precio,
+        cantidad,
+        usuario: user._id
+      })
 
-router.put('/api/product/:id', uploadImg.single('imgUrl'), async (req, res) => {
-  try {
-    const id = req.params.id
-    const { nombre, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad, estado } = req.body
-    const data = {
-      nombre,
-      imgUrl: '/uploads/' + req.file.filename || '',
-      descripcion,
-      categoria,
-      talla,
-      color,
-      fit,
-      genero,
-      temporada,
-      precio,
-      cantidad,
-      estado
+      const createProduct = await data.save()
+      user.productos = user.productos.concat(data._id)
+      await user.save()
+      res.status(201).json(createProduct)
+    } else {
+      res.status(403).json({ error: 'Access denied: insufficient permissions' })
     }
-
-    console.log(id)
-    console.log(req.body)
-
-    // const updateProduct = await Product.findByIdAndUpdate(id, data, {new: true})
-    const updateProduct = await Product.findByIdAndUpdate(id, data, { new: true })
-    res.status(200).json(updateProduct)
   } catch (error) {
     console.error('Error al crear el producto:', error)
     res.status(500).json({ error: error.message })
   }
 })
 
-router.delete('/api/product/:id', async (req, res) => {
+router.put('/api/product/:id', authorizationVerification, uploadImg.single('imgUrl'), async (req, res) => {
   try {
     const id = req.params.id
+    const userRole = req.userRole
 
-    const deleteProduct = await Product.findByIdAndDelete(id)
-    res.status(200).json(deleteProduct)
+    if (userRole === 'Administrador') {
+      const { nombre, descripcion, categoria, talla, color, fit, genero, temporada, precio, cantidad, estado } = req.body
+      const data = {
+        nombre,
+        imgUrl: '/uploads/' + req.file.filename || '',
+        descripcion,
+        categoria,
+        talla,
+        color,
+        fit,
+        genero,
+        temporada,
+        precio,
+        cantidad,
+        estado
+      }
+      const updateProduct = await Product.findByIdAndUpdate(id, data, { new: true })
+      res.status(200).json(updateProduct)
+    } else {
+      res.status(403).json({ error: 'Access denied: insufficient permissions' })
+    }
+  } catch (error) {
+    console.error('Error al crear el producto:', error)
+    res.status(500).json({ error: error.message })
+  }
+})
+
+router.delete('/api/product/:id', authorizationVerification, async (req, res) => {
+  try {
+    const userRole = req.userRole
+    if (userRole === 'Administrador') {
+      const id = req.params.id
+      const deleteProduct = await Product.findByIdAndDelete(id)
+      res.status(200).json(deleteProduct)
+    } else {
+      res.status(403).json({ error: 'Access denied: insufficient permissions' })
+    }
   } catch (error) {
     console.error('Error al crear el producto:', error)
     res.status(500).json({ error: error.message })
